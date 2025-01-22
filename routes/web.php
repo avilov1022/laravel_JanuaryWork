6f4fe70a1ce3c9148b3c1c7ee27441d717fefaed
Студенческая <@rbiu.local>
2025-01-22 11:13:04 +0500

    second_commit

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,9 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+use App\Http\Middleware\Admin;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\ReportController;
 
 
 
@@ -27,6 +19,11 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::middleware((Admin::class))->group(function(){
+    Route::get('/admin', [AdminController::class, 'index']) -> name('admin.index');
+    Route::get('/admin/reports', [ReportController::class, 'index']) -> name('reports.index');
 });
 
 require __DIR__.'/auth.php';
